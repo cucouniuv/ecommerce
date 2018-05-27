5940c0c92d1489b5bc9c6ef1dec5a90f303c2286
Luis Olivetti <luuisf92@gmail.com>
2018-05-27 14:03:35 -0300

    Criação da classe Page

--- index.php
+++ index.php
@@ -4,16 +4,18 @@ require_once("vendor/autoload.php");
 
 $app = new \Slim\Slim();
 
+use \Hcode\Page;
+
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-	
-	$results = $sql->select("select * from tb_users");
-	
-	echo json_encode($results);
 
+	$page = new Page();
+	$page->setTpl("index");
+    
+	//$sql = new Hcode\DB\Sql();	
+	//$results = $sql->select("select * from tb_users");	
+	//echo json_encode($results);
 });
 
 $app->run();
